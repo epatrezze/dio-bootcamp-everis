292cad35bc83f81b5fc9f56bef1d08b87400564e
bannzp <evandro.patrezze@hotmail.com>
2021-08-11 20:12:06 -0300

    implements JDBC CRUD.

---
+++ src/main/java/jdbc/QueriesExecution.java
@@ -0,0 +1,16 @@
+package jdbc;
+
+import java.util.List;
+
+public class QueriesExecution {
+    public static void main(String[] args) {
+
+     AlunoDAO alunoDAO = new AlunoDAO();
+
+     List<Aluno> alunos = alunoDAO.list();
+
+     alunos.stream().forEach(System.out::println);
+
+
+    }
+}
